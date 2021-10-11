51f62a8bf89f772086a967ce09076e95999fb46f
Md.Ebna amir foysal <ebnaamirfoysal@gmail.com>
2021-10-11 06:15:54 +0600

    5Th Deployment

--- resources/views/admin/billing.blade.php
+++ resources/views/admin/billing.blade.php
@@ -24,22 +24,25 @@
                         <button class="ui btn btn-primary mini offsettop5 btn-edit float-right"><i class="ui icon plus"></i>{{ __("বিল আপডেট করুন") }}</button>
                         <button onclick="location.href='/admin/billing/generate'" class="ui btn btn-secondary mini offsettop5 float-right">{{ __("এই মাসের বিল তৈরি করুন") }}</button>
                     </p>
-<hr>
+                    <hr>
+
+                    <div class="row">
+
+                        <div class="col-md-12">
 
                     <form action="{{ url('admin/billing') }}" method="post" accept-charset="utf-8" class="ui small form form-filter" id="filterform">
                         @csrf
-                        <div class="inline three fields">
-                            <div class="three wide field">
+                        <div class="two fields">
+                            <div class="field">
                                 <select name="billing_time" id="billing_time" class="ui search dropdown getid">
                                     <option value="">{{ __("বিলিং সময়সীমা") }}</option>
                                     <option value="1">{{ __("চলতি মাস") }}</option>
                                     <option value="2">{{ __("আগের মাস") }}</option>
                                     <option value="3">{{ __("শুরু থেকে") }}</option>
-  
                                 </select>
                             </div>
 
-                            <div class="three wide field">
+                            <div class="field">
                                 <select name="billing_status" id="billing_status" class="ui search dropdown getid">
                                     <option value="">{{ __("বিলিং স্ট্যাটাস") }}</option>
                                     <option value="1">{{ __("বিল পরিশোধিত।") }}</option>
@@ -47,87 +50,100 @@
                                 </select>
                             </div>
 
-                            <input type="hidden" name="emp_id" value="">
-                            <button type="submit" name="submit" class="ui icon button blue small inline-button"><i class="ui icon filter alternate"></i> {{ __("ফিল্টার") }}</button>
+                            <div class="field">
+                                <input type="hidden" name="emp_id" value="">
+                                <button type="submit" name="submit" class="ui icon button blue small inline-button"><i class="ui icon filter alternate"></i> {{ __("ফিল্টার") }}</button>
+                            </div>
+
                         </div>
                     </form>
 
-                <table width="100%" class="table" id="dataTables-example" data-order='[[ 0, "asc" ]]'>
-                    <thead>
+                        </div>
 
-                    <tr><th colspan="6"></th></tr>
+                    </div>
 
-                    <tr class="text-center">
-                        <th colspan="2" class="table-primary">মোট বিল : {{ $total_bill }} টাকা </th>
-                        <th  colspan="2" class="table-danger">চলতি মাসে মোট বিল: {{ $total_this_month }} টাকা</th>
-                        <th colspan="2" class="table-info">বিল বাকী আছে : {{ $paid_this_month }} টাকা</th>
-                    
-                    </tr>
 
-                    <tr><th colspan="6"></th></tr>
 
-                    <tr>
-                            <th>{{ __("গ্রাহকের কার্ড নং") }}</th>
-                            <th>{{ __("গ্রাহকের নাম") }}</th>
-                            <th>{{ __("বিল মাস") }}</th>
-                            <th>{{ __("বিল বছর") }}</th>
-                            <th>{{ __("বিলের পরিমান")}}</th>
-                            <th>{{ __("বিলিং স্ট্যাটাস")}}</th>
+                    <table width="100%" class="table" id="dataTables-example" data-order='[[ 0, "asc" ]]'>
+                        <thead>
 
-                    </tr>
+                            <tr>
+                                <th colspan="6"></th>
+                            </tr>
 
-                    </thead>
-                    <tbody>
-                        @isset($billingData)
-                        @foreach ($billingData as $val)
-                        <tr>
-                            <td>{{ $val->client_id}}</td>
-                            <td>{{ $val->client_name}}</td>
-                            @if($val->bill_month=="1")
-                            <td>জানুয়ারী</td>
-                            @elseif($val->bill_month=="2")
-                            <td>ফেব্রুয়ারী</td>
-                            @elseif($val->bill_month=="3")
-                            <td>মার্চ</td>
-                            @elseif($val->bill_month=="4")
-                            <td>এপ্রিল</td>
-                            @elseif($val->bill_month=="5")
-                            <td>মে</td>
-                            @elseif($val->bill_month=="6")
-                            <td>জুন</td>
-                            @elseif($val->bill_month=="7")
-                            <td>জুলায়</td>
-                            @elseif($val->bill_month=="8")
-                            <td>আগষ্ট</td>
-                            @elseif($val->bill_month=="9")
-                            <td>সেপ্টেম্বর</td>
-                            @elseif($val->bill_month=="10")
-                            <td>অক্টোবর</td>
-                            @elseif($val->bill_month=="11")
-                            <td>নভেমম্বর</td>
-                            @elseif($val->bill_month=="12")
-                            <td>ডিসেম্বর</td>
-                            @endif
+                            <tr class="text-center">
+                                <th colspan="2" class="table-primary">মোট বিল : {{ $total_bill }} টাকা </th>
+                                <th colspan="2" class="table-danger">চলতি মাসে মোট বিল: {{ $total_this_month }} টাকা</th>
+                                <th colspan="2" class="table-info">বিল বাকী আছে : {{ $paid_this_month }} টাকা</th>
 
-                            <td>{{ $val->bill_year }}</td>
-                            <td>{{ $val->bill_amount }}</td>
+                            </tr>
 
-                            @if($val->billing_status=="1")
-                            <td class="text-primary">পরিশোধিত</td>
-                            @elseif($val->billing_status=="0")
-                            <td class="text-danger">বাকী </td>
-                            @endif
+                            <tr>
+                                <th colspan="6"></th>
+                            </tr>
 
-                        </tr>
-                        @endforeach
-                        @endisset
-                    </tbody>
-                </table>
+                            <tr>
+                                <th>{{ __("গ্রাহকের কার্ড নং") }}</th>
+                                <th>{{ __("গ্রাহকের নাম") }}</th>
+                                <th>{{ __("বিল মাস") }}</th>
+                                <th>{{ __("বিল বছর") }}</th>
+                                <th>{{ __("বিলের পরিমান")}}</th>
+                                <th>{{ __("বিলিং স্ট্যাটাস")}}</th>
+
+                            </tr>
+
+                        </thead>
+                        <tbody>
+                            @isset($billingData)
+                            @foreach ($billingData as $val)
+                            <tr>
+                                <td>{{ $val->client_id}}</td>
+                                <td>{{ $val->client_name}}</td>
+                                @if($val->bill_month=="1")
+                                <td>জানুয়ারী</td>
+                                @elseif($val->bill_month=="2")
+                                <td>ফেব্রুয়ারী</td>
+                                @elseif($val->bill_month=="3")
+                                <td>মার্চ</td>
+                                @elseif($val->bill_month=="4")
+                                <td>এপ্রিল</td>
+                                @elseif($val->bill_month=="5")
+                                <td>মে</td>
+                                @elseif($val->bill_month=="6")
+                                <td>জুন</td>
+                                @elseif($val->bill_month=="7")
+                                <td>জুলায়</td>
+                                @elseif($val->bill_month=="8")
+                                <td>আগষ্ট</td>
+                                @elseif($val->bill_month=="9")
+                                <td>সেপ্টেম্বর</td>
+                                @elseif($val->bill_month=="10")
+                                <td>অক্টোবর</td>
+                                @elseif($val->bill_month=="11")
+                                <td>নভেমম্বর</td>
+                                @elseif($val->bill_month=="12")
+                                <td>ডিসেম্বর</td>
+                                @endif
+
+                                <td>{{ $val->bill_year }}</td>
+                                <td>{{ $val->bill_amount }}</td>
+
+                                @if($val->billing_status=="1")
+                                <td class="text-primary">পরিশোধিত</td>
+                                @elseif($val->billing_status=="0")
+                                <td class="text-danger">বাকী </td>
+                                @endif
+
+                            </tr>
+                            @endforeach
+                            @endisset
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
 
-</div>
+    </div>
 
 </div>
 
