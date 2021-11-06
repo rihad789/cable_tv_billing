95df69554688bdabf58deffdbf7656cbbcfbbab7
Md.Ebna amir foysal <ebnaamirfoysal@gmail.com>
2021-11-07 03:40:06 +0600

    Final Deployement

--- resources/views/layouts/employee.blade.php
+++ resources/views/layouts/employee.blade.php
@@ -7,9 +7,10 @@
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/img/favicon-16x16.png') }}">
+
+    <!-- <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/img/favicon-16x16.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/img/favicon-32x32.png') }}">
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/img/favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/img/favicon.ico') }}"> -->
 
     @yield('meta')
 
